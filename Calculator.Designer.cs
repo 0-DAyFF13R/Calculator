9d66eafa3b9456fa806d0444a82eb9f14b7ae930
Саша Анашин <sasha.anash2016@gmail.com>
2020-05-16 18:33:02 +0300

    Version 0.5

--- Calculator.Designer.cs
+++ Calculator.Designer.cs
@@ -523,7 +523,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(605, 419);
+            this.ClientSize = new System.Drawing.Size(338, 419);
             this.Controls.Add(this.buttonSciFi);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button19);
